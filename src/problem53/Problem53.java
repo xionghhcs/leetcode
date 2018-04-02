87f2fcb50bd5809cc2676fa068fda62384022c56
xionghhcs <xionghhcs@163.com>
2018-04-02 12:15:33 +0800

    问题53，解法3提交，线性时间解法

--- src/problem53/Problem53.java
+++ src/problem53/Problem53.java
@@ -10,7 +10,9 @@ package problem53;
 
 public class Problem53 {
     public static void main(String[] args) {
-
+        int[] nums = new int[]{-1, -2, -3};
+        Solution so = new Solution();
+        System.out.println(so.maxSubArray(nums));
     }
 }
 
@@ -19,36 +21,35 @@ public class Problem53 {
 * */
 class Solution {
     public int maxSubArray(int[] nums) {
-        return findMaxSubArray(nums, 0, nums.length-1);
+        return findMaxSubArray(nums, 0, nums.length - 1);
     }
 
-    private int findMaxSubArray(int [] A, int low, int high){
-        if(low == high){
+    private int findMaxSubArray(int[] A, int low, int high) {
+        if (low == high) {
             return A[low];
-        }
-        else{
+        } else {
             int mid = (low + high) / 2;
             int left_sum = findMaxSubArray(A, low, mid);
-            int right_sum = findMaxSubArray(A, mid+1, high);
+            int right_sum = findMaxSubArray(A, mid + 1, high);
             int cross_sum = findMaxCrossingSubArray(A, low, mid, high);
             return Math.max(Math.max(left_sum, right_sum), cross_sum);
         }
     }
 
-    private int findMaxCrossingSubArray(int A[], int low, int mid, int high){
+    private int findMaxCrossingSubArray(int A[], int low, int mid, int high) {
         int left_sum = Integer.MIN_VALUE;
         int sum = 0;
-        for(int i = mid; i>=low;i--){
+        for (int i = mid; i >= low; i--) {
             sum += A[i];
-            if(sum > left_sum){
+            if (sum > left_sum) {
                 left_sum = sum;
             }
         }
-        int right_sum=Integer.MIN_VALUE;
+        int right_sum = Integer.MIN_VALUE;
         sum = 0;
-        for(int j= mid+1;j<=high;j++){
+        for (int j = mid + 1; j <= high; j++) {
             sum += A[j];
-            if(sum > right_sum){
+            if (sum > right_sum) {
                 right_sum = sum;
             }
         }
@@ -61,18 +62,39 @@ class Solution {
 *
 * 在leetcode上，这题直接用暴力是会超时的。
 * */
-class Solution2{
+class Solution2 {
     public int maxSubArray(int[] nums) {
         int max_sum = Integer.MIN_VALUE;
-        for(int i=0;i<nums.length;i++){
+        for (int i = 0; i < nums.length; i++) {
             int sum = 0;
-            for(int j=i;j<nums.length;j++){
+            for (int j = i; j < nums.length; j++) {
                 sum += nums[j];
-                if(sum > max_sum){
-                    max_sum= sum;
+                if (sum > max_sum) {
+                    max_sum = sum;
                 }
             }
         }
         return max_sum;
+    }
+}
+
+/*
+* 非递归、线性求解方法
+* */
+
+class Solution3 {
+    public int maxSubArray(int[] nums) {
+        int res = Integer.MIN_VALUE;
+        int sum = 0;
+        for(int i=0;i<nums.length;i++){
+            sum += nums[i];
+            if(sum > res){
+                res = sum;
+            }
+            if(sum < 0){
+                sum = 0;
+            }
+        }
+        return res;
     }
 }
