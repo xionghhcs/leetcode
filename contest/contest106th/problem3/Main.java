616c84cd5f672df64fe0c12b30fdb48bd6610147
xionghhcs <xionghhcs@163.com>
2018-10-14 16:44:20 +0800

    contest106th

--- contest/contest106th/problem3/Main.java
+++ contest/contest106th/problem3/Main.java
@@ -13,7 +13,6 @@ public class Main {
 class Solution {
     public int threeSumMulti(int[] A, int target) {
         Arrays.sort(A);
-        System.out.println(A);
         int res = 0;
         for(int i =0;i<A.length; i++){
             if (A[i] > target){
@@ -29,7 +28,7 @@ class Solution {
                     }
                     if(A[i] + A[j] + A[k] == target){
                         res += 1;
-                        // res = res % (1000000000 + 7);
+                         res = res % (1000000000 + 7);
                     }
                 }
             }
@@ -54,5 +53,49 @@ class Solution2 {
             map.put(Integer.valueOf(A[i]), Integer.valueOf(v));
         }
         return (int)ans;
+    }
+}
+
+class Solution3{
+    /*
+    * @author : zhantons(leetcode id)
+    *
+    * There is not necessary to sort the array of A
+    * */
+    public int threeSumMulti(int[] A, int target) {
+        int ans = 0;
+        int mod = 1000000007;
+        for(int i=0;i<A.length; i++){
+            HashMap<Integer, Integer> map = new HashMap<>();
+            map.put(A[A.length - 1], 1);
+            for(int j= A.length - 2; j > i; j--){
+                ans = (ans + map.getOrDefault(target - A[i] - A[j], 0)) % mod;
+                map.put(A[j], map.getOrDefault(A[j], 0) + 1);
+            }
+        }
+        return ans;
+    }
+}
+
+class Solution4{
+    /*
+    * @Author: 	szp14
+    *
+    * DP
+    * */
+    private int [][] dp;
+    public int threeSumMulti(int[] A, int target) {
+        int ans = 0;
+        int mod = 1000000007;
+        dp = new int[3][305];
+        for(int i=0;i<A.length;i++){
+            for(int j=2;j>=1;j--){
+                for(int k=target; k>=A[i]; k--){
+                    dp[j][k] = (dp[j][k] + dp[j -1][k - A[i]]) % mod;
+                }
+            }
+            dp[0][A[i]] = (dp[0][A[i]] + 1) % mod;
+        }
+        return ans;
     }
 }
