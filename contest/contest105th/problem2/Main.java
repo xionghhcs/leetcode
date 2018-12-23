685b5c01fc5fa207c1e293aaf4c7338e6029792c
xionghhcs <xionghhcs@163.com>
2018-12-23 16:38:43 +0800

    delete no use files

--- contest/contest105th/problem2/Main.java
+++
@@ -1,7 +0,0 @@
-package contest105th.problem2;
-
-public class Main {
-    public static void main(String[] args) {
-
-    }
-}
--- contest/contest106th/problem4/Main.java
+++
@@ -1,4 +0,0 @@
-package contest106th.problem4;
-
-public class Main {
-}
