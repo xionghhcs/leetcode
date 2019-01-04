e480655c6d3f37adf65ad37f9688f0396ce5f6e1
xionghhcs <xionghhcs@163.com>
2019-01-05 00:01:06 +0800

    add easy problems

--- problem_python/p202.py
+++ problem_python/p202.py
@@ -4,3 +4,14 @@ class Solution:
         :type n: int
         :rtype: bool
         """
+        tmp = n
+        while tmp >= 10:
+            ans = []
+            while tmp >0:
+                r = tmp % 10
+                tmp = tmp // 10
+                ans.append(r ** 2)
+            tmp = sum(ans)
+            if tmp == 1:
+                return True
+        return True if tmp in (1,7) else False
